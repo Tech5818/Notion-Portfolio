8be220ac17a19abb8a8f98d1bc6688d5cbd01f9e
Tech5818 <pqow5818@gmail.com>
2024-01-08 18:16:17 +0900

    :sparkles: GNB tab메뉴 구현

--- src/app/_components/server/header/GNB.tsx
+++ src/app/_components/server/header/GNB.tsx
@@ -1,11 +1,34 @@
+'use client';
+
 import { List, ListItem } from '@chakra-ui/react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export const GNB = () => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const tabs = params.get('tabs');
+
+  useEffect(() => {
+    console.log(tabs);
+  });
+
   return (
     <>
       <List listStyleType='none' display='flex' gap='20px'>
-        <ListItem fontSize='22px'>홈</ListItem>
-        <ListItem fontSize='22px'>포트폴리오</ListItem>
+        <ListItem
+          fontSize='22px'
+          fontWeight={tabs === null || tabs === 'home' ? 'bold' : 'nomal'}
+        >
+          홈
+        </ListItem>
+        <ListItem
+          fontSize='22px'
+          fontWeight={tabs === 'portfolio' ? 'bold' : 'nomal'}
+        >
+          포트폴리오
+        </ListItem>
       </List>
     </>
   );
