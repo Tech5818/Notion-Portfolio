86b44158a8fca7f129207ec8ce390fd62bee6d0d
Tech5818 <pqow5818@gmail.com>
2024-01-09 16:36:22 +0900

    :sparkles: GNB로 qeury stirng이동

--- src/app/_components/server/header/GNB.tsx
+++ src/app/_components/server/header/GNB.tsx
@@ -1,17 +1,24 @@
 'use client';
 
 import { List, ListItem } from '@chakra-ui/react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams, usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 
 export const GNB = () => {
   const router = useRouter();
   const params = useSearchParams();
+  const path = usePathname();
 
   const tabs = params.get('tabs');
 
+  const handleNavigate = (where: string) => {
+    router.push(`${path}?tabs=${where}`);
+  };
   useEffect(() => {
-    console.log(tabs);
+    console.log(params.entries());
+    for (const [key, value] of params.entries()) {
+      console.log(`${key} : ${value}`);
+    }
   });
 
   return (
@@ -20,12 +27,18 @@ export const GNB = () => {
         <ListItem
           fontSize='22px'
           fontWeight={tabs === null || tabs === 'home' ? 'bold' : 'nomal'}
+          onClick={() => {
+            handleNavigate('home');
+          }}
         >
           홈
         </ListItem>
         <ListItem
           fontSize='22px'
           fontWeight={tabs === 'portfolio' ? 'bold' : 'nomal'}
+          onClick={() => {
+            handleNavigate('portfolio');
+          }}
         >
           포트폴리오
         </ListItem>
